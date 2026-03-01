4a8b152a65bf97696f26abef2a06702a1981ddec
carlosalejandroperezceron <89228822+CarlosAlejandroPerezCeron@users.noreply.github.com>
2026-03-01 01:03:52 -0500

    Variables for region and instance types

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "ai-platform-cluster"
+}
+
+variable "gpu_instance_type" {
+  default = "g5.xlarge"
+}
+
+variable "cpu_instance_type" {
+  default = "t3.medium"
+}
